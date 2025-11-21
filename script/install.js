074266d7c747ba1ac1ae649e621f651dc304c14d
Rikelmy Lopes <rikelmylopes899@gmail.com>
2025-11-21 09:38:32 -0300

    build: install comando mais rapido

--- script/install.js
+++ script/install.js
@@ -2,7 +2,7 @@ import { spawn } from "child_process";
 import { APP_CWD, APP_WEB_UI } from "./constants.js";
 
 (async () => {
-  await spawn("npm", ["install"], { shell: true, stdio: "inherit", cwd: APP_CWD });
+  spawn("npm", ["install"], { shell: true, stdio: "inherit", cwd: APP_CWD });
 
-  await spawn("npm", ["install"], { shell: true, stdio: "inherit", cwd: APP_WEB_UI });
+  spawn("npm", ["install"], { shell: true, stdio: "inherit", cwd: APP_WEB_UI });
 })();
