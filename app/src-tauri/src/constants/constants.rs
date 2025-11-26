055423e5137282a82fe11bbe6232618e5e3c00e5
Rikelmy Lopes <rikelmylopes899@gmail.com>
2025-11-26 13:36:26 -0300

    refac: alterado o caminho de webapp

--- app/src-tauri/src/constants/constants.rs
+++ app/src-tauri/src/constants/constants.rs
@@ -1,2 +1,2 @@
 pub const WEBAPP_RESOURCE_PATH_PROD: &str = "resources/webapp";
-pub const WEBAPP_RESOURCE_PATH_DEV: &str = "../web-ui/webapp";
+pub const WEBAPP_RESOURCE_PATH_DEV: &str = "../webapp";
--- app/web-ui/vite.config.ts
+++ app/web-ui/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     // Adjust output path in development to avoid restarts caused by changes in 'resources'
-    outDir: mode === "development" ? "./webapp" : "../src-tauri/resources/webapp",
+    outDir: mode === "development" ? "../webapp" : "../src-tauri/resources/webapp",
     emptyOutDir: true,
   },
 }));
