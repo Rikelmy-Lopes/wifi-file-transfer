332629ab5ebfbe246ebc0e4aa63c2ee40c27ba84
Rikelmy Lopes <rikelmylopes899@gmail.com>
2026-01-16 16:03:54 -0300

    add classes to handle config

--- app/src/config/ConfigManager.ts
+++ app/src/config/ConfigManager.ts
@@ -4,7 +4,12 @@ export class ConfigManager {
   public load(): Promise<Config> {
     throw new Error();
   }
+
   public save(config: Config): Promise<boolean> {
+    throw new Error();
+  }
+
+  public resetToDefaults(): void {
     throw new Error();
   }
 }
